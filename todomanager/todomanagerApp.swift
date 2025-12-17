5ba998238cbac96f00f791e99cc908c0d488dbab
ottoxin <hxin9@wisc.edu>
2025-12-17 14:47:17 -0600

    Initial Commit

---
+++ todomanager/todomanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  todomanagerApp.swift
+//  todomanager
+//
+//  Created by HAOHANG XIN on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct todomanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
